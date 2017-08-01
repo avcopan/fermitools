a2e71e0db7dbbac4585d620724c4210ca2528019
avcopan <avcopan@gmail.com>
2017-08-01 12:10:45 -0400

    Removes examples b/c too difficult to maintain for now.  Tests should suffice.

--- examples/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import coefficients_rohf, corr_energy_rmp2, energy_rhf, energy_uhf
-
-__all__ = ['coefficients_rohf', 'corr_energy_rmp2', 'energy_rhf', 'energy_uhf']
--- examples/corr/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import math, rmp2, ump2
-
-__all__ = ['math', 'rmp2', 'ump2']
