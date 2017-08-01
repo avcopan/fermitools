ab47d30c0b3de6ae9efc9ac121f0a3e1bb7649b4
avcopan <avcopan@gmail.com>
2017-08-01 13:00:11 -0400

    Adds test for math.spinorb.sort()

--- tests/math/test_spinorb.py
+++ tests/math/test_spinorb.py
@@ -31,3 +31,28 @@ def test__expand():
 def test__ab2ov():
     assert spinorb.ab2ov(dim=4, na=2, nb=2) == (0, 1, 4, 5, 2, 3, 6, 7)
     assert spinorb.ab2ov(dim=5, na=3, nb=2) == (0, 1, 2, 5, 6, 3, 4, 7, 8, 9)
+
+
+def test__transform():
+    a = numpy.array([[1, 1, 0, 0],
+                     [1, 1, 0, 0],
+                     [0, 0, 1, 1],
+                     [0, 0, 1, 1]])
+
+    b = spinorb.sort(a, order=(0, 2, 1, 3), axes=(0,))
+    assert_array_equal(b, [[1, 1, 0, 0],
+                           [0, 0, 1, 1],
+                           [1, 1, 0, 0],
+                           [0, 0, 1, 1]])
+
+    c = spinorb.sort(a, order=(0, 2, 1, 3), axes=(1,))
+    assert_array_equal(c, [[1, 0, 1, 0],
+                           [1, 0, 1, 0],
+                           [0, 1, 0, 1],
+                           [0, 1, 0, 1]])
+
+    d = spinorb.sort(a, order=(0, 2, 1, 3), axes=(0, 1))
+    assert_array_equal(d, [[1, 0, 1, 0],
+                           [0, 1, 0, 1],
+                           [1, 0, 1, 0],
+                           [0, 1, 0, 1]])
