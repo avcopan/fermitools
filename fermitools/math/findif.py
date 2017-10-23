00e6f613828eb67009c8d8f41704362db7859b6e
avcopan <avcopan@gmail.com>
2017-10-23 17:21:26 -0400

    Further generalizes central differencer to allow multiple step sizes -- hasn't been subjected to much testing!

--- fermitools/math/findif.py
+++ fermitools/math/findif.py
@@ -9,8 +9,8 @@ def central_difference(f, x, step=0.005, nder=1, npts=None):
     :type f: typing.Callable
     :param x: the point at which to evaluate the derivative
     :type x: float or numpy.ndarrray
-    :param step: the step size
-    :type step: float
+    :param step: step size, or a grid of step sizes corresponding to `x`
+    :type step: float or numpy.ndarray
     :param nder: return the nth derivative
     :type nder: int
     :param npts: the number of grid points, default `nder` + `1` + `nder % 2`
@@ -18,14 +18,17 @@ def central_difference(f, x, step=0.005, nder=1, npts=None):
     """
     if npts is None:
         npts = nder + 1 + nder % 2
+    if numpy.ndim(step) == 0:
+        step = float(step) * numpy.ones_like(x)
+
     weights = scipy.misc.central_diff_weights(Np=npts, ndiv=nder)
 
     def derivative(index):
         dx = numpy.zeros_like(x)
-        dx[index] = step
+        dx[index] = step[index]
         grid = [numpy.array(x) + (k - npts//2) * dx for k in range(npts)]
         vals = tuple(map(f, grid))
-        return numpy.vdot(weights, vals) / (step ** nder)
+        return numpy.vdot(weights, vals) / (step[index] ** nder)
 
     der = tuple(map(derivative, numpy.ndindex(numpy.shape(x))))
     return numpy.reshape(der, numpy.shape(x))
