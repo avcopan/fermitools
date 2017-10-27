5225fa80448058b10fba0a3e0da6b25e6687c05e
avcopan <avcopan@gmail.com>
2017-10-27 16:02:14 -0400

    Adds unravel functions to asym module, and improves the test of the raveling functions.

--- fermitools/math/asym/__init__.py
+++ fermitools/math/asym/__init__.py
@@ -1,5 +1,7 @@
 from .op import antisymmetrize, antisymmetrizer, antisymmetrizer_product
 from .ravel import compound_index, compound_indexer
+from .unravel import unravel_compound_index, compound_index_unraveler
 
 __all__ = ['antisymmetrize', 'antisymmetrizer', 'antisymmetrizer_product',
-           'compound_index', 'compound_indexer']
+           'compound_index', 'compound_indexer',
+           'unravel_compound_index', 'compound_index_unraveler']
--- tests/math/asym/test_ravel.py
+++ tests/math/asym/test_ravel.py
@@ -1,9 +1,19 @@
 from fermitools.math.asym import ravel
 
+import os
 import numpy
+from numpy.testing import assert_almost_equal
+
+data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+UNRAV = numpy.load(os.path.join(data_path, 'unraveled.npy'))
+RAV = numpy.load(os.path.join(data_path, 'raveled.npy'))
 
 
 def test__compound_index():
+    rav = ravel.compound_index(UNRAV, {0: (0, 4), 1: (1, 5), 2: (2, 3, 6)})
+    assert_almost_equal(rav, RAV, decimal=10)
+
+    # Make sure it also works for other orderings
     a = numpy.random.rand(3, 5, 6, 6, 3, 5, 6)
     b0 = ravel.compound_index(a, {0: (0, 4), 1: (1, 5), 2: (2, 3, 6)})
     b1 = ravel.compound_index(a, {0: (0, 4), 2: (1, 5), 1: (2, 3, 6)})
