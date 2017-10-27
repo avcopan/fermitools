83be543bc8f5edd6b82b6383a93591c100886293
avcopan <avcopan@gmail.com>
2017-10-27 09:17:16 -0400

    Adds test scripts where I finally figured out the relationship between numerical and analytic Hessians.

---
+++ scripts/lr_ocepa0_test.py
@@ -0,0 +1,270 @@
+import numpy
+import scipy.linalg as spla
+
+import fermitools
+from fermitools.math.asym import antisymmetrizer_product as asym
+
+import interfaces.psi4 as interface
+from . import ocepa0
+
+
+def second_order_orbital_variation_tensor(h, g, m1, m2):
+    i = numpy.eye(*h.shape)
+    fc = ocepa0.first_order_orbital_variation_matrix(h, g, m1, m2)
+    fcs = (fc + numpy.transpose(fc)) / 2.
+    hc = (+ numpy.einsum('pr,qs->pqrs', h, m1)
+          + numpy.einsum('pr,qs->pqrs', m1, h)
+          - numpy.einsum('pr,qs->pqrs', i, fcs)
+          - numpy.einsum('pr,qs->pqrs', fcs, i)
+          + numpy.einsum('pxry,qxsy->pqrs', g, m2)
+          + numpy.einsum('pxry,qxsy->pqrs', m2, g)
+          - 1./2. * numpy.einsum('psxy,qrxy->pqrs', g, m2)
+          - 1./2. * numpy.einsum('psxy,qrxy->pqrs', m2, g))
+    return hc
+
+
+def diagonal_orbital_hessian(nocc, norb, h, g, m1, m2):
+    o = slice(None, nocc)
+    v = slice(nocc, None)
+    no = nocc
+    nv = norb - nocc
+    h = second_order_orbital_variation_tensor(h, g, m1, m2)
+    a = h[o, v, o, v]
+    return numpy.reshape(a, (no * nv, no * nv))
+
+
+def offdiagonal_orbital_hessian(nocc, norb, h, g, m1, m2):
+    o = slice(None, nocc)
+    v = slice(nocc, None)
+    no = nocc
+    nv = norb - nocc
+    h = second_order_orbital_variation_tensor(h, g, m1, m2)
+    b = -numpy.transpose(h[o, v, v, o], (0, 1, 3, 2))
+    return numpy.reshape(b, (no * nv, no * nv))
+
+
+def diagonal_amplitude_hessian(foo, fvv, goooo, govov, gvvvv):
+    no, nv, _, _ = govov.shape
+    ndoubles = no * (no - 1) * nv * (nv - 1) // 4
+    io = numpy.eye(*foo.shape)
+    iv = numpy.eye(*fvv.shape)
+    a = (+ asym('2/3|4/5|6/7')(
+               numpy.einsum('ik,jl,ac,bd->ijabklcd', io, io, fvv, iv))
+         - asym('0/1|4/5|6/7')(
+               numpy.einsum('ik,jl,ac,bd->ijabklcd', foo, io, iv, iv))
+         + asym('4/5')(
+               numpy.einsum('ik,jl,abcd->ijabklcd', io, io, gvvvv))
+         + asym('6/7')(
+               numpy.einsum('ijkl,ac,bd->ijabklcd', goooo, iv, iv))
+         - asym('0/1|2/3|4/5|6/7')(
+               numpy.einsum('ik,jcla,bd->ijabklcd', io, govov, iv)))
+    a_cmp = fermitools.math.asym.compound_index(a, {0: (0, 1), 1: (2, 3),
+                                                    2: (4, 5), 3: (6, 7)})
+    return numpy.reshape(a_cmp, (ndoubles, ndoubles))
+
+
+def diagonal_mixed_hessian(o, v, g, t2):
+    no, _, nv, _ = t2.shape
+    nsingles = no * nv
+    ndoubles = no * (no - 1) * nv * (nv - 1) // 4
+    io = numpy.eye(no)
+    iv = numpy.eye(nv)
+    a = (+ asym('0/1')(
+               numpy.einsum('abic,jk->ijabkc', g[v, v, o, v], io))
+         - asym('2/3')(
+               numpy.einsum('akij,bc->ijabkc', g[v, o, o, o], iv))
+         + asym('0/1|2/3')(
+               numpy.einsum('imae,mbec,jk->ijabkc', t2, g[o, v, v, v], io))
+         - asym('0/1|2/3')(
+               numpy.einsum('imae,mkej,bc->ijabkc', t2, g[o, o, v, o], iv))
+         - 1./2 * asym('0/1')(
+               numpy.einsum('mnab,mnic,jk->ijabkc', t2, g[o, o, o, v], io))
+         + 1./2 * asym('2/3')(
+               numpy.einsum('ijef,akef,bc->ijabkc', t2, g[v, o, v, v], iv))
+         - asym('0/1')(
+               numpy.einsum('imab,mkjc->ijabkc', t2, g[o, o, o, v]))
+         + asym('2/3')(
+               numpy.einsum('ijae,bkec->ijabkc', t2, g[v, o, v, v])))
+    a_cmp = fermitools.math.asym.compound_index(a, {0: (0, 1), 1: (2, 3)})
+    return numpy.reshape(a_cmp, (ndoubles, nsingles))
+
+
+def offdiagonal_mixed_hessian(o, v, g, t2):
+    no, _, nv, _ = t2.shape
+    nsingles = no * nv
+    ndoubles = no * (no - 1) * nv * (nv - 1) // 4
+    b = (- numpy.einsum('ijec,abek->ijabkc', t2, g[v, v, v, o])
+         + numpy.einsum('mkab,mcij->ijabkc', t2, g[o, v, o, o])
+         + asym('0/1')(
+               numpy.einsum('imab,mcjk->ijabkc', t2, g[o, v, o, o]))
+         - asym('2/3')(
+               numpy.einsum('ijae,bcek->ijabkc', t2, g[v, v, v, o]))
+         + asym('0/1|2/3')(
+               numpy.einsum('ikae,bcje->ijabkc', t2, g[v, v, o, v]))
+         - asym('0/1|2/3')(
+               numpy.einsum('imac,bmjk->ijabkc', t2, g[v, o, o, o])))
+    b_cmp = fermitools.math.asym.compound_index(b, {0: (0, 1), 1: (2, 3)})
+    return numpy.reshape(b_cmp, (ndoubles, nsingles))
+
+
+def orbital_property_gradient(o, v, p, m1):
+    t = (numpy.dot(p, m1) - numpy.dot(m1, p))[o, v]
+    return numpy.ravel(t)
+
+
+def amplitude_property_gradient(poo, pvv, t2):
+    no, _, nv, _ = t2.shape
+    t = (+ asym('2/3')(
+               numpy.einsum('ac,ijcb->ijab', pvv, t2))
+         - asym('0/1')(
+               numpy.einsum('ik,kjab->ijab', poo, t2)))
+    t_cmp = fermitools.math.asym.compound_index(t, {0: (0, 1), 1: (2, 3)})
+    return numpy.ravel(t_cmp)
+
+
+def static_response_vector(a, b, t):
+    """solve for the static response vector
+
+    :param a: diagonal orbital hessian
+    :type a: numpy.ndarray
+    :param b: off-diagonal orbital hessian
+    :type b: numpy.ndarray
+    :param t: property gradient vector(s)
+    :type t: numpy.ndarray
+    :returns: the response vector(s), (x + y) = 2 * (a + b)^-1 * t
+    :rtype: numpy.ndarray
+    """
+    return spla.solve(a + b, 2 * t, sym_pos=True)
+
+
+def static_linear_response_function(t, r):
+    """the linear response function, evaluated at zero field strength (=static)
+
+    :param t: property gradient vector(s)
+    :type t: numpy.ndarray
+    :param r: the response vector(s), (x + y) = 2 * (a + b)^-1 * t
+    :type r: numpy.ndarray
+    :returns: the response function(s)
+    :rtype: float or numpy.ndarray
+    """
+    return numpy.dot(numpy.transpose(t), r)
+
+
+def spectrum(a, b):
+    w2 = spla.eigvals(numpy.dot(a + b, a - b))
+    return numpy.array(sorted(numpy.sqrt(w2.real)))
+
+
+def tamm_dancoff_spectrum(a):
+    return spla.eigvalsh(a)
+
+
+def main():
+    CHARGE = +1
+    SPIN = 1
+    BASIS = 'sto-3g'
+    LABELS = ('O', 'H', 'H')
+    COORDS = ((0.000000000000,  0.000000000000, -0.143225816552),
+              (0.000000000000,  1.638036840407,  1.136548822547),
+              (0.000000000000, -1.638036840407,  1.136548822547))
+
+    # Spaces
+    na = fermitools.chem.elec.count_alpha(LABELS, CHARGE, SPIN)
+    nb = fermitools.chem.elec.count_beta(LABELS, CHARGE, SPIN)
+    nocc = na + nb
+
+    # Integrals
+    nbf = interface.integrals.nbf(BASIS, LABELS)
+    norb = 2 * nbf
+    h_ao = interface.integrals.core_hamiltonian(BASIS, LABELS, COORDS)
+    r_ao = interface.integrals.repulsion(BASIS, LABELS, COORDS)
+
+    h_aso = fermitools.math.spinorb.expand(h_ao, brakets=((0, 1),))
+    r_aso = fermitools.math.spinorb.expand(r_ao, brakets=((0, 2), (1, 3)))
+    g_aso = r_aso - numpy.transpose(r_aso, (0, 1, 3, 2))
+
+    # Orbitals
+    ac, bc = interface.hf.unrestricted_orbitals(BASIS, LABELS, COORDS,
+                                                CHARGE, SPIN)
+    c_unsrt = spla.block_diag(ac, bc)
+    sortvec = fermitools.math.spinorb.ab2ov(dim=nbf, na=na, nb=nb)
+    c_unsrt = spla.block_diag(ac, bc)
+    c = fermitools.math.spinorb.sort(c_unsrt, order=sortvec, axes=(1,))
+
+    # Solve OCEPA0
+    en_nuc = fermitools.chem.nuc.energy(labels=LABELS, coords=COORDS)
+    t2_guess = numpy.zeros((nocc, nocc, norb-nocc, norb-nocc))
+    en_elec, c, t2 = ocepa0.solve_ocepa0(norb=norb, nocc=nocc, h_aso=h_aso,
+                                         g_aso=g_aso, c_guess=c,
+                                         t2_guess=t2_guess, niter=200,
+                                         e_thresh=1e-14, r_thresh=1e-12,
+                                         print_conv=True)
+    en_tot = en_elec + en_nuc
+    print("Total energy:")
+    print('{:20.15f}'.format(en_tot))
+
+    # Build the diagonal orbital and amplitude Hessian
+    o = slice(None, nocc)
+    v = slice(nocc, None)
+    h = fermitools.math.transform(h_aso, {0: c, 1: c})
+    g = fermitools.math.transform(g_aso, {0: c, 1: c, 2: c, 3: c})
+    f = ocepa0.fock(h[o, o], h[v, v], g[o, o, o, o], g[o, v, o, v])
+    m1_ref = ocepa0.singles_reference_density(norb=norb, nocc=nocc)
+    m1_cor = ocepa0.singles_correlation_density(t2)
+    m1 = m1_ref + m1_cor
+    k2 = ocepa0.doubles_cumulant(t2)
+    m2 = ocepa0.doubles_density(m1_ref, m1_cor, k2)
+
+    a_orb = diagonal_orbital_hessian(nocc, norb, h, g, m1, m2)
+    a_mix = diagonal_mixed_hessian(o, v, g, t2)
+    a_amp = diagonal_amplitude_hessian(f[o, o], f[v, v], g[o, o, o, o],
+                                       g[o, v, o, v], g[v, v, v, v])
+
+    b_orb = offdiagonal_orbital_hessian(nocc, norb, h, g, m1, m2)
+    b_mix = offdiagonal_mixed_hessian(o, v, g, t2)
+    b_amp = numpy.zeros_like(a_amp)
+
+    from numpy.testing import assert_almost_equal
+
+    # assert_almost_equal(numpy.diag(alpha), -en_df2, decimal=11)
+
+    # Test the orbital and amplitude gradients
+    import functools
+
+    t1 = numpy.zeros((nocc, norb-nocc))
+    t2_flat = numpy.ravel(
+            fermitools.math.asym.compound_index(t2, {0: (0, 1), 1: (2, 3)}))
+    en_func = ocepa0.electronic_energy_functional(norb=norb, nocc=nocc,
+                                                  h_aso=h_aso, g_aso=g_aso,
+                                                  c=c)
+
+    en_orb = functools.partial(en_func, t2_flat=t2_flat)
+    en_amp = functools.partial(en_func, t1)
+
+    # Test the orbital and amplitude Hessians
+    en_dx2 = fermitools.math.central_difference(en_orb, t1, step=0.05, nder=2,
+                                                npts=9)
+    en_dt2 = fermitools.math.central_difference(en_amp, t2_flat, step=0.05,
+                                                nder=2, npts=9)
+
+    print("Numerical orbital Hessian diagonal:")
+    print(numpy.ravel(en_dx2))
+
+    print("A_orb + B_orb diagonal:")
+    print(numpy.diag(a_orb + b_orb).round(9))
+
+    print("Ratio:")
+    print(numpy.diag(a_orb + b_orb) / numpy.ravel(en_dx2))
+
+    print("Numerical amplitude Hessian diagonal:")
+    print(numpy.ravel(en_dt2))
+
+    print("A_amp + B_amp diagonal:")
+    print(numpy.diag(a_amp + b_amp).round(9))
+
+    print("Ratio:")
+    print(numpy.diag(a_amp + b_amp) / numpy.ravel(en_dt2))
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/lr_scf_test.py
@@ -0,0 +1,166 @@
+import numpy
+import scipy.linalg as spla
+
+import fermitools
+
+import interfaces.psi4 as interface
+from . import scf
+
+
+def second_order_orbital_variation_tensor(h, g, m1, m2):
+    i = numpy.eye(*h.shape)
+    fc = scf.first_order_orbital_variation_matrix(h, g, m1, m2)
+    fcs = (fc + numpy.transpose(fc)) / 2.
+    hc = (+ numpy.einsum('pr,qs->pqrs', h, m1)
+          + numpy.einsum('pr,qs->pqrs', m1, h)
+          - numpy.einsum('pr,qs->pqrs', i, fcs)
+          - numpy.einsum('pr,qs->pqrs', fcs, i)
+          + numpy.einsum('pxry,qxsy->pqrs', g, m2)
+          + numpy.einsum('pxry,qxsy->pqrs', m2, g)
+          - 1./2. * numpy.einsum('psxy,qrxy->pqrs', g, m2)
+          - 1./2. * numpy.einsum('psxy,qrxy->pqrs', m2, g))
+    return hc
+
+
+def diagonal_orbital_hessian(nocc, norb, h, g, m1, m2):
+    o = slice(None, nocc)
+    v = slice(nocc, None)
+    no = nocc
+    nv = norb - nocc
+    h = second_order_orbital_variation_tensor(h, g, m1, m2)
+    a = h[o, v, o, v]
+    return numpy.reshape(a, (no * nv, no * nv))
+
+
+def offdiagonal_orbital_hessian(nocc, norb, h, g, m1, m2):
+    o = slice(None, nocc)
+    v = slice(nocc, None)
+    no = nocc
+    nv = norb - nocc
+    h = second_order_orbital_variation_tensor(h, g, m1, m2)
+    b = -numpy.transpose(h[o, v, v, o], (0, 1, 3, 2))
+    return numpy.reshape(b, (no * nv, no * nv))
+
+
+def orbital_property_gradient(o, v, p, m1):
+    t = (numpy.dot(p, m1) - numpy.dot(m1, p))[o, v]
+    return numpy.ravel(t)
+
+
+def static_response_vector(a, b, t):
+    """solve for the static response vector
+
+    :param a: diagonal orbital hessian
+    :type a: numpy.ndarray
+    :param b: off-diagonal orbital hessian
+    :type b: numpy.ndarray
+    :param t: property gradient vector(s)
+    :type t: numpy.ndarray
+    :returns: the response vector(s), (x + y) = 2 * (a + b)^-1 * t
+    :rtype: numpy.ndarray
+    """
+    return spla.solve(a + b, 2 * t, sym_pos=True)
+
+
+def static_linear_response_function(t, r):
+    """the linear response function, evaluated at zero field strength (=static)
+
+    :param t: property gradient vector(s)
+    :type t: numpy.ndarray
+    :param r: the response vector(s), (x + y) = 2 * (a + b)^-1 * t
+    :type r: numpy.ndarray
+    :returns: the response function(s)
+    :rtype: float or numpy.ndarray
+    """
+    return numpy.dot(numpy.transpose(t), r)
+
+
+def spectrum(a, b):
+    w2 = spla.eigvals(numpy.dot(a + b, a - b))
+    return numpy.array(sorted(numpy.sqrt(w2.real)))
+
+
+def tamm_dancoff_spectrum(a):
+    return spla.eigvalsh(a)
+
+
+def main():
+    CHARGE = 0
+    SPIN = 0
+    BASIS = 'sto-3g'
+    LABELS = ('O', 'H', 'H')
+    COORDS = ((0.000000000000,  0.000000000000, -0.143225816552),
+              (0.000000000000,  1.638036840407,  1.136548822547),
+              (0.000000000000, -1.638036840407,  1.136548822547))
+
+    # Spaces
+    na = fermitools.chem.elec.count_alpha(LABELS, CHARGE, SPIN)
+    nb = fermitools.chem.elec.count_beta(LABELS, CHARGE, SPIN)
+    nocc = na + nb
+
+    # Integrals
+    nbf = interface.integrals.nbf(BASIS, LABELS)
+    norb = 2 * nbf
+    h_ao = interface.integrals.core_hamiltonian(BASIS, LABELS, COORDS)
+    r_ao = interface.integrals.repulsion(BASIS, LABELS, COORDS)
+
+    h_aso = fermitools.math.spinorb.expand(h_ao, brakets=((0, 1),))
+    r_aso = fermitools.math.spinorb.expand(r_ao, brakets=((0, 2), (1, 3)))
+    g_aso = r_aso - numpy.transpose(r_aso, (0, 1, 3, 2))
+
+    # Orbitals
+    ac, bc = interface.hf.unrestricted_orbitals(BASIS, LABELS, COORDS,
+                                                CHARGE, SPIN)
+    c_unsrt = spla.block_diag(ac, bc)
+    sortvec = fermitools.math.spinorb.ab2ov(dim=nbf, na=na, nb=nb)
+    c_unsrt = spla.block_diag(ac, bc)
+    c = fermitools.math.spinorb.sort(c_unsrt, order=sortvec, axes=(1,))
+
+    en_nuc = fermitools.chem.nuc.energy(labels=LABELS, coords=COORDS)
+    en_elec, c = scf.solve_scf(norb=norb, nocc=nocc, h_aso=h_aso, g_aso=g_aso,
+                               c_guess=c, niter=200, e_thresh=1e-14,
+                               r_thresh=1e-12, print_conv=200)
+    en_tot = en_elec + en_nuc
+    print("Total energy:")
+    print('{:20.15f}'.format(en_tot))
+
+    # Evalute the dipole polarizability as a linear response function
+    h = fermitools.math.transform(h_aso, {0: c, 1: c})
+    g = fermitools.math.transform(g_aso, {0: c, 1: c, 2: c, 3: c})
+    m1 = scf.singles_density(norb=norb, nocc=nocc)
+    m2 = scf.doubles_density(m1)
+    a_orb = diagonal_orbital_hessian(nocc, norb, h, g, m1, m2)
+    b_orb = offdiagonal_orbital_hessian(nocc, norb, h, g, m1, m2)
+
+    w_rpa = spectrum(a_orb, b_orb)
+
+    from numpy.testing import assert_almost_equal
+    w_rpa_ref = [0.2851637170, 0.2851637170, 0.2851637170, 0.2997434467,
+                 0.2997434467, 0.2997434467, 0.3526266606, 0.3526266606,
+                 0.3526266606, 0.3547782530, 0.3651313107, 0.3651313107,
+                 0.3651313107, 0.4153174946, 0.5001011401, 0.5106610509,
+                 0.5106610509, 0.5106610509, 0.5460719086, 0.5460719086,
+                 0.5460719086, 0.5513718846, 0.6502707118, 0.8734253708,
+                 1.1038187957, 1.1038187957, 1.1038187957, 1.1957870714,
+                 1.1957870714, 1.1957870714, 1.2832053178, 1.3237421886,
+                 19.9585040647, 19.9585040647, 19.9585040647, 20.0109471551,
+                 20.0113074586, 20.0113074586, 20.0113074586, 20.0504919449]
+
+    assert_almost_equal(w_rpa, w_rpa_ref, decimal=10)
+
+    # Test derivatives
+    t1_0 = numpy.zeros((nocc, norb-nocc))
+    en_func = scf.electronic_energy_functional(norb=norb, nocc=nocc,
+                                               h_aso=h_aso, g_aso=g_aso, c=c)
+
+    en_dx2_real = fermitools.math.central_difference(en_func, t1_0, step=0.05,
+                                                     nder=2, npts=9)
+
+    print(numpy.ravel(en_dx2_real).round(9))
+    print(numpy.diag(a_orb + b_orb).round(9))
+
+    print((numpy.diag(a_orb + b_orb) / numpy.ravel(en_dx2_real)).round(9))
+
+
+if __name__ == '__main__':
+    main()
