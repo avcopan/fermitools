f94e04c0ef050f53d3bd5e1ed0d91c693ca2e1ea
avcopan <avcopan@gmail.com>
2018-03-24 14:53:25 -0400

    Corrects the formula for transition strengths.

--- fermitools/lr/util.py
+++ fermitools/lr/util.py
@@ -7,4 +7,4 @@ def transition_dipole(s, d, pg, x, y):
     m = subtract(s, d)
     norms = numpy.diag(numpy.dot(x.T, m(x)) - numpy.dot(y.T, m(y)))
     t = numpy.dot(x.T, pg) + numpy.dot(y.T, pg)
-    return norms[:, None] * t * t
+    return t * t / norms[:, None]
