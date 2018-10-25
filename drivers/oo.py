8c035183209c02d7e4f14b5a18a3d24c4f1389bf
avcopan <avcopan@gmail.com>
2018-10-25 12:59:32 -0500

    Fixes the nuclear repulsion energy.  Thanks to Roujing Peng.

--- drivers/oo.py
+++ drivers/oo.py
@@ -1,10 +1,10 @@
+""" main drivers for the OO methods
+"""
 import numpy
 import fermitools
 
 
-def hf_orbitals(labels, coords, charge, spin, basis, angstrom, interface):
-    coords = numpy.divide(coords, 0.52917720859) if angstrom else coords
-
+def hf_orbitals(labels, coords, charge, spin, basis, interface):
     na = fermitools.chem.elec.count_alpha(labels, charge, spin)
     nb = fermitools.chem.elec.count_beta(labels, charge, spin)
     nbf = interface.integrals.nbf(basis, labels)
@@ -23,41 +23,37 @@ def hf_orbitals(labels, coords, charge, spin, basis, angstrom, interface):
     return co, cv, no, nv
 
 
-def integrals(basis, labels, coords, angstrom, interface):
-    coords = numpy.divide(coords, 0.52917720859) if angstrom else coords
-
+def integrals(basis, labels, coords, interface):
     h_ao = interface.integrals.core_hamiltonian(basis, labels, coords)
     r_ao = interface.integrals.repulsion(basis, labels, coords)
     p_ao = interface.integrals.dipole(basis, labels, coords)
-
-    # Doesn't really belong here, but ...
-    en_nuc = fermitools.chem.nuc.energy(labels=labels, coords=coords)
-    print("\nnuclear replusion energy: {:20.15f}".format(en_nuc))
-
     return h_ao, r_ao, p_ao
 
 
 def energy(method, labels, coords, charge, spin, basis, angstrom=False,
            maxiter=100, rthresh=1e-10, diis_start=3, diis_nvec=20,
            interface=None):
-    h_ao, r_ao, p_ao = integrals(basis, labels, coords, angstrom, interface)
+    coords = numpy.divide(coords, 0.52917720859) if angstrom else coords
+
+    h_ao, r_ao, p_ao = integrals(basis, labels, coords, interface)
     co_guess, cv_guess, no, nv = hf_orbitals(
-            labels, coords, charge, spin, basis, angstrom, interface)
+            labels, coords, charge, spin, basis, interface)
     t2_guess = numpy.zeros((no, no, nv, nv))
+
     en_nuc = fermitools.chem.nuc.energy(labels=labels, coords=coords)
     print("\nnuclear replusion energy: {:20.15f}".format(en_nuc))
 
     if method.lower() == 'ocepa0':
         en_elec, co, cv, t2, info = fermitools.oo.ocepa0.solve(
-                h_ao=h_ao, r_ao=r_ao, co_guess=co_guess, cv_guess=cv_guess,
-                t2_guess=t2_guess, maxiter=maxiter, rthresh=rthresh,
-                diis_start=diis_start, diis_nvec=diis_nvec, print_conv=True)
+            h_ao=h_ao, r_ao=r_ao, co_guess=co_guess, cv_guess=cv_guess,
+            t2_guess=t2_guess, maxiter=maxiter, rthresh=rthresh,
+            diis_start=diis_start, diis_nvec=diis_nvec, print_conv=True)
         fermitools.oo.ocepa0.compute_property(p_ao, co, cv, t2)
     elif method.lower() == 'odc12':
         en_elec, co, cv, t2, info = fermitools.oo.odc12.solve(
-                h_ao=h_ao, r_ao=r_ao, co_guess=co_guess, cv_guess=cv_guess,
-                t2_guess=t2_guess, maxiter=maxiter, rthresh=rthresh,
-                diis_start=diis_start, diis_nvec=diis_nvec, print_conv=True)
+            h_ao=h_ao, r_ao=r_ao, co_guess=co_guess, cv_guess=cv_guess,
+            t2_guess=t2_guess, maxiter=maxiter, rthresh=rthresh,
+            diis_start=diis_start, diis_nvec=diis_nvec, print_conv=True)
         fermitools.oo.odc12.compute_property(p_ao, co, cv, t2)
     else:
         raise Exception("Method {:s} does not exist.".format(str(method)))
@@ -69,9 +65,12 @@ def spectrum(method, labels, coords, charge, spin, basis, angstrom=False,
              nroot=1, nconv=None, nguess=None, maxdim=None, maxiter=100,
              rthresh=1e-5, oo_maxiter=200, oo_rthresh=1e-10, diis_start=3,
              diis_nvec=20, disk=False, blsize=None, interface=None):
-    h_ao, r_ao, p_ao = integrals(basis, labels, coords, angstrom, interface)
+
+    coords = numpy.divide(coords, 0.52917720859) if angstrom else coords
+    
+    h_ao, r_ao, p_ao = integrals(basis, labels, coords, interface)
     co_guess, cv_guess, no, nv = hf_orbitals(
-            labels, coords, charge, spin, basis, angstrom, interface)
+            labels, coords, charge, spin, basis, interface)
     t2_guess = numpy.zeros((no, no, nv, nv))
 
     if method.lower() == 'ocepa0':
